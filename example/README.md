96ee4950f36fb78834ad03ba1a46e85e9d137841
Friedrich Lindenberg <friedrich@pudo.org>
2017-10-19 18:11:52 +0200

    sample scraper and runtime environment

---
+++ example/README.md
@@ -0,0 +1,6 @@
+# Memorious example project
+
+This folder can be used as an example template of a memorious deployment.
+Copy it into it's own git repository as a starting point, then add your
+own crawlers and scrapers as needed.
+
---
+++ example/setup.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from setuptools import setup, find_packages
+
+setup(
+    name='crawlers',
+    version=datetime.utcnow().date().isoformat(),
+    classifiers=[],
+    keywords='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['memorious',],
+    entry_points={},
+    tests_require=[]
+)
