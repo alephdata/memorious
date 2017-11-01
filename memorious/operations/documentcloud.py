61ebe61d25a3766364d3963210155aa074c8833b
Friedrich Lindenberg <friedrich@pudo.org>
2017-11-01 08:05:39 +0100

    linearize dc crawler.

--- memorious/operations/documentcloud.py
+++ memorious/operations/documentcloud.py
@@ -1,6 +1,5 @@
 import os
 from urlparse import urljoin
-from itertools import count
 from pycountry import languages
 
 DEFAULT_HOST = 'https://documentcloud.org/'
@@ -11,31 +10,31 @@ def documentcloud_query(context, data):
     host = context.get('host', DEFAULT_HOST)
     instance = context.get('instance', DEFAULT_INSTANCE)
     query = context.get('query')
+    page = data.get('page', 1)
 
     search_url = urljoin(host, 'search/documents.json')
-    for page in count(1):
-        res = context.http.get(search_url, params={
-            'q': query,
-            'per_page': 100,
-            'page': page
-        })
-        documents = res.json.get('documents', [])
-        if not len(documents):
-            break
+    res = context.http.get(search_url, params={
+        'q': query,
+        'per_page': 100,
+        'page': page
+    })
+    documents = res.json.get('documents', [])
+    for document in documents:
+        data['foreign_id'] = '%s:%s' % (instance, document.get('id'))
+        data['url'] = document.get('pdf_url')
+        data['source_url'] = document.get('canonical_url')
+        data['title'] = document.get('title')
+        data['author'] = document.get('author')
+        data['file_name'] = os.path.basename(document.get('pdf_url'))
+        data['mime_type'] = 'application/pdf'
 
-        for document in documents:
-            data['foreign_id'] = '%s:%s' % (instance, document.get('id'))
-            data['url'] = document.get('pdf_url')
-            data['source_url'] = document.get('canonical_url')
-            data['title'] = document.get('title')
-            data['author'] = document.get('author')
-            data['file_name'] = os.path.basename(document.get('pdf_url'))
-            data['mime_type'] = 'application/pdf'
+        # lang_code = document.get('language')
+        # try:
+        #     lang = languages.get(alpha_3=lang_code)
+        #     data['languages'] = [lang.alpha_2]
+        # except Exception:
+        #     context.emit_warning("Unknown language code: %s" % lang_code)
+        context.emit(rule='pass', data=data)
 
-            try:
-                lang = languages.get(alpha_3=document.get('language'))
-                data['countries'] = [lang.alpha_2]
-            except Exception as exc:
-                context.log.exception(exc)
-
-            context.emit(rule='pass', data=data)
+    if len(documents):
+        context.recurse(data={'page': page + 1})
