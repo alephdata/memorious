0ce6510c530b4dbda9c5d822aaf605b4ebd72153
Friedrich Lindenberg <friedrich@pudo.org>
2017-12-09 09:52:25 +0100

    use JSON consistently

--- memorious/model/common.py
+++ memorious/model/common.py
@@ -12,12 +12,10 @@ class TextJSON(TypeDecorator):
     impl = TEXT
 
     def process_bind_param(self, value, dialect):
-        if value is not None:
-            return json.dumps(value)
+        return json.dumps(value)
 
     def process_result_value(self, value, dialect):
-        if value is not None:
-            return json.loads(value)
+        return json.loads(value)
 
 
 JSON = TextJSON().with_variant(JSONB, 'postgresql')
