a9dcf994a133858c3d357bbc06127b7202a57200
rhiaro <amy@rhiaro.co.uk>
2017-12-13 13:46:26 +0100

    Formatted date

--- memorious/helpers/dates.py
+++ memorious/helpers/dates.py
@@ -8,3 +8,7 @@ def parse_date(text, format_hint=None):
         return datetime.strptime(text, format_hint)
     else:
         return dateparser.parse(text)
+
+
+def iso_date(text, format_hint=None):
+    return parse_date(text, format_hint).isoformat()
