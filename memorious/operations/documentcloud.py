9906d7a37b2f628afb5b05540aeda73e520b9990
Friedrich Lindenberg <friedrich@pudo.org>
2018-02-17 18:58:36 +0100

    Fix document cloud languages

--- memorious/operations/documentcloud.py
+++ memorious/operations/documentcloud.py
@@ -5,6 +5,31 @@ from six.moves.urllib.parse import urljoin
 DEFAULT_HOST = 'https://documentcloud.org/'
 DEFAULT_INSTANCE = 'documentcloud'
 
+LANGUAGES = {
+    "ara": "ar",
+    "zho": "zh",
+    "tra": "zh",
+    "hrv": "hr",
+    "dan": "da",
+    "nld": "nl",
+    "eng": "en",
+    "fra": "fr",
+    "deu": "de",
+    "heb": "he",
+    "hun": "hu",
+    "ind": "id",
+    "ita": "it",
+    "jpn": "ja",
+    "kor": "ko",
+    "nor": "no",
+    "por": "pt",
+    "ron": "ro",
+    "rus": "ru",
+    "spa": "es",
+    "swe": "sv",
+    "ukr": "uk"
+}
+
 
 def documentcloud_query(context, data):
     host = context.get('host', DEFAULT_HOST)
@@ -29,13 +54,16 @@ def documentcloud_query(context, data):
             'file_name': os.path.basename(document.get('pdf_url')),
             'mime_type': 'application/pdf'
         }
-        # if document.get('language'):
-        #     data['languages'] = [document.get('language')]
+
+        lang = LANGUAGES.get(document.get('language'))
+        if lang is not None:
+            data['languages'] = [lang]
 
         published = document.get('created_at')
         if published is not None:
             dt = datetime.strptime(published, '%b %d, %Y')
             doc['published_at'] = dt.isoformat()
+
         context.emit(data=doc)
 
     if len(documents):
