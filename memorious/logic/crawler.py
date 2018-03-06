alephdata/memorious

commit e3f0431041fbb25d58e8954de29e7c62ace7e39e

Make reporting module more independent.

memorious/logic/crawler.py
@@ -6,12 +6,10 @@ import time
 from datetime import timedelta, datetime
 from importlib import import_module
 
-import blinker
-
-from memorious import settings
-from memorious.signals import signals
-from memorious.core import session, local_queue, connect_redis
+from memorious import settings, signals
+from memorious.core import session, local_queue
 from memorious.model import Tag, Event, Result
+from memorious.reporting import get_last_run
 from memorious.logic.context import handle
 from memorious.logic.stage import CrawlerStage
 from memorious.helpers.dates import parse_date
@@ -51,14 +49,6 @@ class Crawler(object):
         for name, stage in self.config.get('pipeline', {}).items():
             self.stages[name] = CrawlerStage(self, name, stage)
 
-    def last_run(self):
-        with connect_redis() as conn:
-            if conn:
-                last_run = conn.get(self.name+":last_run")
-                if last_run:
-                    return parse_date(last_run)
-            return None
-
     def check_due(self):
         """Check if the last execution of this crawler is older than
         the scheduled interval."""
@@ -66,7 +56,7 @@ class Crawler(object):
             return False
         if self.delta is None:
             return False
-        last_run = self.last_run()
+        last_run = get_last_run(self)
         if last_run is None:
             return True
         now = datetime.utcnow()
@@ -74,32 +64,13 @@ class Crawler(object):
             return True
         return False
 
-    def get_op_count(self):
-        """Total operations performed for this crawler"""
-        with connect_redis() as conn:
-            if conn:
-                total_ops = conn.get(self.name+":total_ops")
-                if total_ops:
-                    return int(total_ops)
-            return None
-
-    def is_running(self):
-        """Is the crawler currently running?"""
-        with connect_redis() as conn:
-            if conn:
-                active_ops = conn.get(self.name)
-                if active_ops and int(active_ops) > 0:
-                    return True
-            return False
-
     def flush(self):
         """Delete all run-time data generated by this crawler."""
         Tag.delete(self.name)
         Event.delete(self.name)
         Result.delete(self.name)
         session.commit()
-        flushed_signal = blinker.signal(signals.CRAWLER_FLUSHED)
-        flushed_signal.send(self)
+        signals.crawler_flush.send(self)
 
     def run(self, incremental=None):
         """Queue the execution of a particular crawler."""

memorious/cli.py
@@ -3,7 +3,8 @@ import logging
 from tabulate import tabulate
 
 from memorious import settings
-from memorious.core import manager, upgrade_db, ensure_db
+from memorious.core import manager, load_extensions
+from memorious.core import upgrade_db, ensure_db
 
 log = logging.getLogger(__name__)
 
@@ -23,6 +24,7 @@ def cli(debug, cache, incremental):
     if settings.DEBUG:
         logging.basicConfig(level=logging.DEBUG)
     ensure_db()
+    load_extensions()
 
 
 @cli.command()

memorious/core.py
@@ -1,7 +1,9 @@
 import os
 import logging
 from contextlib import contextmanager
+from pkg_resources import iter_entry_points
 
+import redis
 import dataset
 import storagelayer
 from celery import Celery
@@ -13,7 +15,6 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.local import LocalProxy
 from raven import Client
 from raven.contrib.celery import register_signal, register_logger_signal
-import redis
 
 from memorious import settings
 from memorious.logic.queue import CrawlerExecutionQueue
@@ -50,16 +51,6 @@ celery.conf.update(
 redis_pool = redis.ConnectionPool(
     host=settings.REDIS_HOST, port=settings.REDIS_PORT
 )
-
-
-@contextmanager
-def connect_redis():
-    if settings.REDIS_HOST:
-        conn = redis.Redis(connection_pool=redis_pool)
-        yield conn
-    else:
-        yield None
-
 
 # set up a task queue using a Queue if celery is set to eager mode.
 local_queue = CrawlerExecutionQueue()
@@ -121,3 +112,9 @@ def upgrade_db():
     with session.bind.begin() as connection:
         alembic_cfg.attributes['connection'] = connection
         command.upgrade(alembic_cfg, "head")
+
+
+def load_extensions():
+    for ep in iter_entry_points('memorious.plugins'):
+        func = ep.load()
+        func()

memorious/logic/context.py
@@ -11,8 +11,6 @@ from datetime import datetime, timedelta
 from contextlib import contextmanager
 import time
 
-import blinker
-
 from memorious.core import manager, storage, celery, session
 from memorious.core import datastore, local_queue
 from memorious.model import Result, Tag, Event
@@ -20,8 +18,7 @@ from memorious.exc import StorageFileMissing
 from memorious.logic.http import ContextHttp
 from memorious.logic.rate_limit import rate_limiter, RateLimitException
 from memorious.util import make_key, random_filename
-from memorious import settings
-from memorious.signals import signals
+from memorious import settings, signals
 
 
 class Context(object):
@@ -70,19 +67,16 @@ class Context(object):
         so."""
 
         try:
-            start_signal = blinker.signal(signals.CRAWLER_RUNNING)
-            start_signal.send(self)
+            signals.operation_start.send(self)
             self.log.debug('Running: %s', self.stage.name)
-            res = self.stage.method(self, data)
-            return res
+            return self.stage.method(self, data)
         except Exception as exc:
             # this should clear results and tags created by this op
             # TODO: should we also use transactions on the datastore?
             session.rollback()
             self.emit_exception(exc)
         finally:
-            stop_signal = blinker.signal(signals.CRAWLER_FINISHED)
-            stop_signal.send(self)
+            signals.operation_end.send(self)
             shutil.rmtree(self.work_path)
             # Save the results and events created in this op
             session.commit()

memorious/logic/manager.py
@@ -13,7 +13,7 @@ class CrawlerManager(object):
     def __init__(self, path):
         self.path = path
 
-        if not os.path.exists(self.path):
+        if not os.path.isdir(self.path):
             log.warning('Crawler config path %s not found.', self.path)
 
         self.crawlers = {}

memorious/logic/rate_limit.py
@@ -1,5 +1,4 @@
 import logging
-import threading
 import contextlib
 import datetime
 
@@ -10,7 +9,6 @@ from memorious.core import redis_pool
 from memorious.exc import RateLimitException
 
 log = logging.getLogger(__name__)
-lock = threading.RLock()
 global_call_log = {}
 
 
@@ -20,8 +18,10 @@ def rate_limiter(context):
     rate_limit = context.stage.rate_limit
     if settings.REDIS_HOST:
         try:
-            with RateLimit(resource=resource, client='memorious',
-                           max_requests=rate_limit, redis_pool=redis_pool):
+            with RateLimit(resource=resource,
+                           client='memorious',
+                           max_requests=rate_limit,
+                           redis_pool=redis_pool):
                 yield
         except TooManyRequests:
             raise RateLimitException

memorious/logic/stage.py
@@ -1,7 +1,5 @@
 from importlib import import_module
 
-from memorious.core import connect_redis
-
 
 class CrawlerStage(object):
     """A single step in a data processing crawler."""
@@ -25,15 +23,6 @@ class CrawlerStage(object):
             package, method = method.rsplit(':', 1)
         module = import_module(package)
         return getattr(module, method)
-
-    def get_op_count(self):
-        """Total operations performed for this stage"""
-        with connect_redis() as conn:
-            if conn:
-                total_ops = conn.get(self.crawler.name + ":" + self.name)
-                if total_ops:
-                    return int(total_ops)
-            return None
 
     def __repr__(self):
         return '<CrawlerStage(%r, %s)>' % (self.crawler, self.name)

memorious/reporting.py
@@ -1,0 +1,124 @@
+import logging
+import datetime
+
+import redis
+
+from memorious import settings
+from memorious.core import redis_pool
+from memorious.signals import operation_start
+from memorious.signals import operation_end
+from memorious.signals import crawler_flush
+from memorious.helpers.dates import parse_date
+
+log = logging.getLogger(__name__)
+
+
+def connect_redis():
+    if not settings.REDIS_HOST:
+        raise RuntimeError("No $MEMORIOUS_REDIS_HOST is set.")
+    return redis.Redis(connection_pool=redis_pool)
+
+
+def log_operation_start(context):
+    conn = connect_redis()
+    crawler_name = context.crawler.name
+    stage_name = context.stage.name
+    now = datetime.datetime.utcnow()
+    conn.incr(crawler_name)
+    conn.incr(crawler_name + ":" + stage_name)
+    conn.incr(crawler_name + ":total_ops")
+    conn.set(crawler_name + ":last_run", now)
+    if not conn.sismember(crawler_name + ":runs", context.run_id):
+        conn.sadd(crawler_name + ":runs", context.run_id)
+        conn.set("run:" + context.run_id + ":start", now)
+    conn.incr("run:" + context.run_id)
+    conn.incr("run:" + context.run_id + ":total_ops")
+
+
+def log_operation_end(context):
+    conn = connect_redis()
+    crawler_name = context.crawler.name
+    conn.decr(crawler_name)
+    conn.decr("run:" + context.run_id)
+    if conn.get("run:" + context.run_id) == "0":
+        now = datetime.datetime.utcnow()
+        conn.set("run:" + context.run_id + ":end", now)
+
+
+def flush_crawler(crawler):
+    conn = connect_redis()
+    crawler_name = crawler.name
+    conn.delete(crawler_name)
+    conn.delete(crawler_name + ":total_ops")
+    conn.delete(crawler_name + ":last_run")
+    for run_id in conn.smembers(crawler_name + ":runs"):
+        conn.delete("run:" + run_id + ":start")
+        conn.delete("run:" + run_id + ":end")
+        conn.delete("run:" + run_id + ":total_ops")
+        conn.delete("run:" + run_id)
+    conn.delete(crawler_name + ":runs")
+    for stage in crawler.stages:
+        conn.delete(crawler_name + ":" + stage)
+
+
+def get_last_run(crawler):
+    if not settings.REDIS_HOST:
+        return None
+    conn = connect_redis()
+    last_run = conn.get(crawler.name+":last_run")
+    if last_run:
+        return parse_date(last_run)
+
+
+def get_crawler_op_count(crawler):
+    """Total operations performed for this crawler"""
+    if not settings.REDIS_HOST:
+        return None
+    conn = connect_redis()
+    total_ops = conn.get(crawler.name+":total_ops")
+    if total_ops:
+        return int(total_ops)
+
+
+def get_stage_op_count(self):
+    """Total operations performed for this stage"""
+    if not settings.REDIS_HOST:
+        return None
+    conn = connect_redis()
+    total_ops = conn.get(self.crawler.name + ":" + self.name)
+    if total_ops:
+        return int(total_ops)
+
+
+def is_running(crawler):
+    """Is the crawler currently running?"""
+    if not settings.REDIS_HOST:
+        return False
+    conn = connect_redis()
+    active_ops = conn.get(crawler.name)
+    if active_ops and int(active_ops) > 0:
+        return True
+    return False
+
+
+def get_crawler_runs(crawler):
+    if not settings.REDIS_HOST:
+        return []
+    conn = connect_redis()
+    runs = []
+    for run_id in conn.smembers(crawler.name + ":runs"):
+        runs.append({
+            'run_id': run_id,
+            'total_ops': conn.get("run:" + run_id + ":total_ops"),
+            'start': parse_date(conn.get("run:" + run_id + ":start")),
+            'end': parse_date(conn.get("run:" + run_id + ":end"))
+        })
+    return runs
+
+
+def init():
+    if settings.REDIS_HOST:
+        log.info("redis available, configuring reporting...")
+        operation_start.connect(log_operation_start)
+        operation_end.connect(log_operation_end)
+        crawler_flush.connect(flush_crawler)

commit 12bab6c368dc19f2534e981cbbb37e3279b7de83

Let reporting deal with redis
Fix bug introduced in e3f0431041fbb25d58e8954de29e7c62ace7e39e
-- crawler no longer has direct access to redis.

memorious/logic/crawler.py
@@ -9,7 +9,7 @@ from importlib import import_module
 from memorious import settings, signals
 from memorious.core import session, local_queue
 from memorious.model import Tag, Event, Result
-from memorious.reporting import get_last_run
+from memorious.reporting import get_last_run, is_running
 from memorious.logic.context import handle
 from memorious.logic.stage import CrawlerStage
 
@@ -108,13 +108,10 @@ class Crawler(object):
 
     def cleanup(self):
         """Run a cleanup method after the crawler finishes running"""
-        with connect_redis() as conn:
-            if conn:
-                active_ops = conn.get(self.name)
-                if not active_ops or int(active_ops) != 0:
-                    log.info("Clean up did not run: Crawler %s has not run or"
+        if is_running(self):
+            log.info("Clean up did not run: Crawler %s"
                              " is currently running" % self.name)
-                    return
+            return
         if self.cleanup_method:
             log.info("Running clean up for %s" % self.name)
             self.cleanup_method(self.cleanup_config["params"])