e49821127a0669640b58472b5e76b38214a8c56a
Tarashish Mishra <sunu0000@gmail.com>
2018-03-14 13:33:57 +0530

    Include migration files in the package

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=[],
     include_package_data=True,
-    package_data={},
+    package_data={'memorious': ['migrate/*', 'migrate/versions/*']},
     zip_safe=False,
     install_requires=[
         'banal',
