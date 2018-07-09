3ce50748b573ae1104e268bc83966a9686a4819c
Friedrich Lindenberg <friedrich@pudo.org>
2018-07-09 23:19:24 +0200

    Use make_key method for redis key.

--- memorious/model/event.py
+++ memorious/model/event.py
@@ -2,6 +2,7 @@ import json
 import logging
 
 from memorious.model.common import Base, pack_now, unpack_datetime
+from memorious.util import make_key
 
 log = logging.getLogger(__name__)
 
@@ -15,46 +16,35 @@ class Event(Base):
     @classmethod
     def save(cls, crawler, stage, level, run_id, error=None, message=None):
         """Create an event, possibly based on an exception."""
-        assert level in cls.LEVELS
         event = {
             'stage': stage.name,
             'level': level,
-            'run_id': run_id,
             'timestamp': pack_now(),
             'error': error,
             'message': message
         }
-        event_data = json.dumps(event)
-        cls.conn.lpush(crawler.name + ":events", event_data)
-        cls.conn.lpush(crawler.name + ":events:" + level, event_data)
-        cls.conn.lpush(
-            crawler.name + ":" + stage.name + ":events", event_data
-        )
-        cls.conn.lpush(
-            crawler.name + ":" + stage.name + ":events:" + level, event_data
-        )
-        cls.conn.lpush(
-            crawler.name + ":" + run_id + ":events", event_data
-        )
-        cls.conn.lpush(
-            crawler.name + ":" + run_id + ":events:" + level, event_data
-        )
+        data = json.dumps(event)
+        cls.conn.lpush(make_key(crawler, "events"), data)
+        cls.conn.lpush(make_key(crawler, "events", level), data)
+        cls.conn.lpush(make_key(crawler, stage, "events"), data)
+        cls.conn.lpush(make_key(crawler, stage.name, "events", level), data)
+        cls.conn.lpush(make_key(crawler, run_id, "events"), data)
+        cls.conn.lpush(make_key(crawler, run_id, "events", level), data)
         return event
 
     @classmethod
     def delete(cls, crawler):
-        cls.conn.delete(crawler.name + ":events")
+        cls.conn.delete(make_key(crawler, "events"))
         for level in cls.LEVELS:
-            cls.conn.delete(crawler.name + ":events:" + level)
+            cls.conn.delete(make_key(crawler, "events", level))
             for stage in crawler.stages:
-                key = crawler.name + ":" + stage + ":events:" + level
-                cls.conn.delete(key)
+                cls.conn.delete(make_key(crawler, stage, "events", level))
 
     @classmethod
     def get_counts(cls, crawler):
         counts = {}
         for level in cls.LEVELS:
-            key = crawler.name + ":events:" + level
+            key = make_key(crawler, "events", level)
             counts[level] = cls.conn.llen(key) or 0
         return counts
 
@@ -62,7 +52,7 @@ class Event(Base):
     def get_stage_counts(cls, crawler, stage):
         counts = {}
         for level in cls.LEVELS:
-            key = crawler.name + ":" + stage.name + ":events:" + level
+            key = make_key(crawler, stage, "events", level)
             counts[level] = cls.conn.llen(key) or 0
         return counts
 
@@ -70,7 +60,7 @@ class Event(Base):
     def get_run_counts(cls, crawler, run_id):
         counts = {}
         for level in cls.LEVELS:
-            key = crawler.name + ":" + run_id + ":events:" + level
+            key = make_key(crawler, run_id, "events", level)
             counts[level] = cls.conn.llen(key) or 0
         return counts
 
@@ -88,26 +78,17 @@ class Event(Base):
 
     @classmethod
     def get_crawler_events(cls, crawler, start, end, level=None):
-        if level:
-            key = crawler.name + ":events:" + level
-        else:
-            key = crawler.name + ":events"
+        key = make_key(crawler, "events", level)
         return cls.event_list(key, start, end)
 
     @classmethod
     def get_stage_events(cls, crawler, stage_name, start, end, level=None):
         """events from a particular stage"""
-        if level:
-            key = crawler.name + ":" + stage_name + ":events:" + level
-        else:
-            key = crawler.name + ":" + stage_name + ":events"
+        key = make_key(crawler, stage_name, "events", level)
         return cls.event_list(key, start, end)
 
     @classmethod
     def get_run_events(cls, crawler, run_id, start, end, level=None):
         """Events from a particular run"""
-        if level:
-            key = crawler.name + ":" + run_id + ":events:" + level
-        else:
-            key = crawler.name + ":" + run_id + ":events"
+        key = make_key(crawler, run_id, "events", level)
         return cls.event_list(key, start, end)
--- memorious/model/tag.py
+++ memorious/model/tag.py
@@ -2,6 +2,7 @@ import json
 import logging
 
 from memorious.model.common import Base
+from memorious.util import make_key
 
 log = logging.getLogger(__name__)
 
@@ -10,25 +11,21 @@ class Tag(Base):
     """A simple key/value table used to store interim results."""
 
     @classmethod
-    def tag(cls, crawler, key):
-        return crawler.name + ":tag:" + key
-
-    @classmethod
     def save(cls, crawler, key, value):
         data = json.dumps(value)
-        cls.conn.set(cls.tag(crawler, key), data, ex=crawler.expire)
+        cls.conn.set(make_key(crawler, "tag", key), data, ex=crawler.expire)
 
     @classmethod
     def find(cls, crawler, key):
-        value = cls.conn.get(cls.tag(crawler, key))
+        value = cls.conn.get(make_key(crawler, "tag", key))
         if value is not None:
             return json.loads(value)
 
     @classmethod
     def exists(cls, crawler, key):
-        return cls.conn.exists(cls.tag(crawler, key))
+        return cls.conn.exists(make_key(crawler, "tag", key))
 
     @classmethod
     def delete(cls, crawler):
-        for key in cls.conn.scan_iter(cls.tag(crawler, '*')):
+        for key in cls.conn.scan_iter(make_key(crawler, "tag", "*")):
             cls.conn.delete(key)
