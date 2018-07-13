efd6161884d408921494781921ef3cd23838dc7e
Tarashish Mishra <sunu0000@gmail.com>
2018-07-13 14:45:42 +0200

    Whether a crawler is running or not is decided slightly differently now

--- memorious/tests/test_reporting.py
+++ memorious/tests/test_reporting.py
@@ -8,20 +8,10 @@ class TestReporting(object):
     def test_operation_reporting(self, crawler, context):
         log_operation_start(context)
         assert crawler.latest_runid == context.run_id
-        assert crawler.is_running
         assert len(list(crawler.runs)) == 1
         assert crawler.op_count == 1
         assert isinstance(crawler.last_run, datetime.datetime)
-
-        # if crawler hasn't finished running or ran recently, cleanup doesn't
-        # do anything
-        crawler.cleanup()
-        assert crawler.is_running
-
         log_operation_end(context)
-        # wait for 10 seconds
-        time.sleep(10)
-        assert crawler.is_running is False
         crawler.flush()
         assert crawler.op_count == 0
         assert len(list(crawler.runs)) == 0
