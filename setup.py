c5a44d361d4165a0d1bada29e8564c206ce5babe
Friedrich Lindenberg <friedrich@pudo.org>
2018-08-10 11:48:16 +0200

    Bump version: 0.7.7 → 0.7.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='memorious',
-    version='0.7.7',
+    version='0.7.8',
     description="A minimalistic, recursive web crawling library for Python.",
     long_description="",
     classifiers=[
