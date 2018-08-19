dbec71a1773ce2b400bee85a7f32e710ed2ae97e
Friedrich Lindenberg <friedrich@pudo.org>
2018-08-19 12:28:30 +0200

    aargh

--- memorious/ui/__init__.py
+++ memorious/ui/__init__.py
@@ -1,17 +1,8 @@
-import logging
-
-from memorious import settings
 from memorious.core import init_memorious
-from memorious.ui.views import app, sentry
+from memorious.ui.views import app
 
 
 init_memorious()
-if settings.SENTRY_DSN:
-    sentry.init_app(app,
-                    dsn=settings.SENTRY_DSN,
-                    logging=True,
-                    level=logging.ERROR)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True)
