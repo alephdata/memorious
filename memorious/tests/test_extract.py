f87791b4ce60d53b6d73723f4f5a19c18298b9ef
Tarashish Mishra <sunu0000@gmail.com>
2018-11-22 12:46:01 +0530

    pytest 4 doesn't like nested fixtures

--- memorious/tests/test_extract.py
+++ memorious/tests/test_extract.py
@@ -14,23 +14,23 @@ def test_extract_7zip(context):
     ]
 
 
-def test_extract_zip():
+def test_extract_zip(context):
     file_path = os.path.realpath(__file__)
     archive_path = os.path.normpath(os.path.join(
         file_path, "../testdata/test.zip"
     ))
     extract_dir = tempfile.mkdtemp(prefix="memorious_test")
-    assert extract_zip(archive_path, extract_dir) == [
+    assert extract_zip(archive_path, extract_dir, context) == [
         os.path.join(extract_dir, "test/a/1.txt")
     ]
 
 
-def test_extract_tar():
+def test_extract_tar(context):
     file_path = os.path.realpath(__file__)
     archive_path = os.path.normpath(os.path.join(
         file_path, "../testdata/test.tar.gz"
     ))
     extract_dir = tempfile.mkdtemp(prefix="memorious_test")
-    assert extract_tar(archive_path, extract_dir, {}) == [
+    assert extract_tar(archive_path, extract_dir, context) == [
         os.path.join(extract_dir, "test/a/1.txt")
     ]
