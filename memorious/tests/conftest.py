a8760bd5838a3676fc064525b0fbeabad821e41e
rhiaro <amy@rhiaro.co.uk>
2018-12-10 18:56:14 +0200

    Fix tests

--- memorious/tests/conftest.py
+++ memorious/tests/conftest.py
@@ -9,8 +9,7 @@ from memorious.logic.context import Context
 from memorious.logic.http import ContextHttp
 
 
-@pytest.fixture(scope="module")
-def crawler_dir():
+def get_crawler_dir():
     file_path = os.path.realpath(__file__)
     crawler_dir = os.path.normpath(os.path.join(
         file_path, "../testdata/config"
@@ -19,31 +18,52 @@ def crawler_dir():
 
 
 @pytest.fixture(scope="module")
-def manager():
+def crawler_dir():
+    return get_crawler_dir()
+
+
+def get_manager():
     manager = CrawlerManager()
-    manager.load_path(crawler_dir())
+    manager.load_path(get_crawler_dir())
     settings._manager = manager
     return manager
 
 
 @pytest.fixture(scope="module")
-def crawler():
-    return manager().get("occrp_web_site")
+def manager():
+    return get_manager()
+
+
+def get_crawler():
+    return get_manager().get("occrp_web_site")
 
 
 @pytest.fixture(scope="module")
-def stage():
-    cr = crawler()
+def crawler():
+    return get_crawler()
+
+
+def get_stage():
+    cr = get_crawler()
     return cr.get(cr.init_stage)
 
 
 @pytest.fixture(scope="module")
-def context():
-    ctx = Context(crawler(), stage(), {"foo": "bar"})
+def stage():
+    return get_stage()
+
+
+def get_context():
+    ctx = Context(get_crawler(), get_stage(), {"foo": "bar"})
     ctx.run_id = str(uuid.uuid4())
     return ctx
 
 
 @pytest.fixture(scope="module")
+def context():
+    return get_context()
+
+
+@pytest.fixture(scope="module")
 def http():
-    return ContextHttp(context())
+    return ContextHttp(get_context())
