6b0d9fe6bc32be774ac47ae228ee33c5284eaf85
Tarashish Mishra <sunu0000@gmail.com>
2019-07-29 20:25:35 +0530

    Pin servicelayer version to prevent frequent breakage

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'normality >= 1.0.0',
         'tabulate',
         'dataset >= 1.0.8',
-        'servicelayer >= 1.8.0',
+        'servicelayer == 1.8.0',
         'urlnormalizer >= 1.2.0',
         'celestial >= 0.2.0',
         'dateparser',
