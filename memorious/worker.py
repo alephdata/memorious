83424cb195391d4ca242c0064ce899c546dbf98c
Tarashish Mishra <sunu0000@gmail.com>
2019-07-29 20:26:24 +0530

    Reintroduce stage wise rate limiting
    It can be used to avoid `QueueTooBigError`

--- memorious/worker.py
+++ memorious/worker.py
@@ -2,6 +2,7 @@ import logging
 from servicelayer.worker import Worker
 
 from memorious.logic.context import Context
+from memorious.logic.rate_limit import get_rate_limit
 from memorious.core import manager, conn
 
 log = logging.getLogger(__name__)
@@ -16,6 +17,16 @@ class MemoriousWorker(Worker):
         context = Context.from_state(state, stage)
         if context.crawler.disabled:
             return
+        if context.stage.rate_limit:
+            resource = "%s:%s" % (context.crawler.name, context.stage.name)
+            rate_limit = get_rate_limit(
+                resource, limit=context.stage.rate_limit
+            )
+            if rate_limit.check():
+                context.execute(data)
+                rate_limit.update()
+            else:
+                Queue.queue(stage, state, data)
         context.execute(data)
 
     def after_task(self, task):
