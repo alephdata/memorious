3a8b1d6aafb419edab2031ccbc9b07c9d31c1d0f
Tarashish Mishra <sunu0000@gmail.com>
2020-01-29 15:49:17 +0530

    Increase default timeout to 6 hours

--- memorious/settings.py
+++ memorious/settings.py
@@ -45,7 +45,7 @@ THREADS = env.to_int('MEMORIOUS_THREADS', min(8, multiprocessing.cpu_count()))
 MAX_SCHEDULED = max(env.to_int('MEMORIOUS_MAX_SCHEDULED', THREADS), 20)
 
 # How many seconds to wait before timing out a crawler
-CRAWLER_TIMEOUT = env.to_int('MEMORIOUS_CRAWLER_TIMEOUT', 3600)
+CRAWLER_TIMEOUT = env.to_int('MEMORIOUS_CRAWLER_TIMEOUT', 3600 * 6)
 
 # Max number of tasks in a stage's task queue
 MAX_QUEUE_LENGTH = env.to_int('MEMORIOUS_MAX_QUEUE_LENGTH', 50000)
