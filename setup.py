5de7a17c6a5938896f63c4ee09a74762e042ae7d
Tarashish Mishra <sunu0000@gmail.com>
2021-04-08 12:37:43 +0530

    Bump version: 2.0.0 → 2.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="memorious",
-    version="2.0.0",
+    version="2.1.0",
     description="A minimalistic, recursive web crawling library for Python.",
     long_description="",
     classifiers=[
