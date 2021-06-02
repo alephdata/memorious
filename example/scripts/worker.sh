04952a9a6dd826f1018e5004fa8b7776f13b245a
Steve Haffenden <steve.haffenden@gmail.com>
2021-06-02 15:13:48 +0200

    Few more tweaks

--- example/scripts/worker.sh
+++ example/scripts/worker.sh
@@ -4,5 +4,4 @@ pip3 install -q -e /crawlers
 # For debugging inside a container, attach a terming and try:
 # python3 /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 memorious/cli.py --debug run book_scraper
 pip3 install debugpy -t /tmp
-
-memorious process
+/bin/bash
