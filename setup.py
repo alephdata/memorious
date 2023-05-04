26232392287f5db0a66b5cb885c40d498d5adc56
Christian Stefanescu <christian.stefanescu@occrp.org>
2023-05-04 13:50:04 +0200

    Fix long_description in package metadata

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
 
 setup(
     name="memorious",
     version="2.6.1",
     description="A minimalistic, recursive web crawling library for Python.",
-    long_description="",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     classifiers=[
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
