a29a47a7b4f19fc1cc6b62c47ff79527cee182e9
Alex Stefanescu <alex.stefanescu@protonmail.com>
2023-08-24 17:44:01 +0300

    Bump servicelayer version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         "tabulate",
         "python-dateutil >= 2.8.2, < 3.0.0",
         "dataset >= 1.6.2",
-        "servicelayer[google,amazon] >= 1.21.1",
+        "servicelayer[google,amazon] >= 1.21.2",
         "pantomime == 0.5.1",
         "alephclient >= 2.3.5",
         "followthemoney >= 3.5.2",
